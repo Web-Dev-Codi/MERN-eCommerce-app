7314ed8e28db124599cd4c089a66ecfcec957471
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-02-28 19:13:16 +0100

    Both back and front end connected

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products.js";
 const port = process.env.PORT || 5000;
 
@@ -8,4 +10,13 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
