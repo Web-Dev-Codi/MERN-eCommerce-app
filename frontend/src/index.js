a8d223f7485e5b6f3de32a9158f5268a74581e14
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-03-02 12:18:44 +0100

    Working on Proxy Bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import `index.css`
 import "./assets/styles/bootstrap.custom.css";
@@ -12,11 +12,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>,
   ),
 );
